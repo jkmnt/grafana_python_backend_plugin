7ff615f8baf6a8aea49adae1d5ba0d9d316bfb39
jkmnt <git@firewood.fastmail.com>
2021-04-22 17:13:53 +0300

    * fake.c app version update

--- fake.c
+++ fake.c
@@ -1,13 +1,13 @@
 #include "stdio.h"
 #include "windows.h"
 
-
 int main(void)
 {
-    printf("\n1|1|tcp|127.0.0.1:50051|grpc\n");
-
-    while (1)
-    {
-        Sleep(1);
-    }
+  printf("1|2|tcp|127.0.0.1:50051|grpc\n");
+  fflush(NULL);
+ 
+  while(1)
+  {
+    Sleep(1);
+  }
 }
